f012f74d0960eafe7317e328346f59c814d660b0
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-10-11 13:57:28 +0100

    fixed bugs with footer and restaurant cards

--- goreview_frontend/src/components/Footer.js
+++ goreview_frontend/src/components/Footer.js
@@ -2,7 +2,8 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <div>
+                <div id="footer-div">
+                    <div>
                     <h4>Connect with us on</h4>
                     <ul className="footer_left">
                         <li><a href="https://www.apple.com/uk/app-store/" >AppStore </a> <img src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-antivirus-software-policy-library-and-information-34.png" height="20px"></img></li>
@@ -11,6 +12,8 @@ const Footer = () => {
                         <li><a href="https://en-gb.facebook.com/">Facebook</a><img src="https://www.freepnglogos.com/uploads/facebook-icons/facebook-icon-transparent-background-3.png"></img></li>
                         <li><a href="https://www.instagram.com/">Instagram</a><img src="https://www.freepnglogos.com/uploads/instagram-icon-png/new-instagram-logo-png-6.png"></img></li>
                     </ul>
+                </div>
+                <div>
                     <ul className="footer_right">
                         <li><a href="#">View all restaurants</a></li>
                         <li><a href="#">Add review</a></li>
@@ -18,7 +21,7 @@ const Footer = () => {
                         <li><a href="https://github.com/joaopulsz/restaurant-review-website">About GOReview</a></li>
                     </ul>
                 </div>
-
+                </div>
                 <div className="footer_middle">
                     <h4>© 2022 GoReview BNTA</h4>
 
