8e707308ada122cd7559a2b2bb6c551a9daaa4ea
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-10-12 14:22:41 +0100

    refactored

--- goreview_frontend/src/components/NewUser.js
+++ goreview_frontend/src/components/NewUser.js
@@ -30,13 +30,13 @@ const NewUser = ({postUser}) => {
         <>
         <h2>Welcome new user!</h2>
         <form onSubmit={handleFormSubmit}>
-            <label for = "userName">Username:</label>
+            <label for = "userName">Username: </label>
             <input type="text" placeholder="user name" name="username" onChange={handleChange} value={user.username}></input>
 
-            <label for = "fullName">Fullname:</label>
+            <label for = "fullName">Full name: </label>
             <input type="text" placeholder="full name" name="fullname" onChange={handleChange} value={user.fullname} ></input>
 
-            <label for = "email">Email:</label>
+            <label for = "email">Email address: </label>
             <input type="text" placeholder="email" name="email" onChange={handleChange} value={user.email}></input>
             <input type="submit"></input>
         </form>
