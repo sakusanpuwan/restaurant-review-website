2c540687c7bf8ef000e9860b511c9c794a5565d4
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-10-12 14:50:13 +0100

    refactored links

--- goreview_frontend/src/components/Navigation.js
+++ goreview_frontend/src/components/Navigation.js
@@ -7,14 +7,15 @@ const Navigation = () => {
                 <h1>GOReview</h1>
                 <div id="nav-links-search-div">
                     <ul id="nav-links">
-                        <li><Link to={`/new-user`} className="nav-link" href="/new-user">Register</Link></li>
+
+                        <Link className="register" to={`/new-user`}><li><a className="nav-link" href="/new-user">Register</a></li></Link>
                         <li><a className="nav-link" href="#projects">My Account</a></li>
                     </ul>
                     <input type="text" name="search" placeholder="Search"></input>
                 </div>
                 <div id="mobile-nav">
                     <ul id="nav-links-mobile">
-                        <li><Link to={'/new-user'} className="nav-link" href="#about-section">Register</Link></li>
+                    <Link className="register" to={`/new-user`}><li><a className="nav-link" href="/new-user">Register</a></li></Link>
                         <li><a className="nav-link" href="#projects">My Account</a></li>
                     </ul>
                     <input type="text" name="search" placeholder="Search"></input>
