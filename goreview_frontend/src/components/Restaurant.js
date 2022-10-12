78224a9ded0cd6b65ca9723f627eaca105b92efe
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-10-12 10:29:04 +0100

    fixed bug with restaurant ratings

--- goreview_frontend/src/components/Restaurant.js
+++ goreview_frontend/src/components/Restaurant.js
@@ -1,18 +1,21 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import RestaurantReviewPage from "./RestaurantReviewPage";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 
 const Restaurant = ({restaurant,reviews}) => {
 
     const route = `/${restaurant.id}`
-    const [rating, setRating] = useState(0);
+    const [rating, setRating] = useState("");
 
 
     const fetchRatingData = async () => {
         const response = await fetch(`http://localhost:8080/restaurants/rating/${restaurant.id}`)
         const ratingData = await response.json();
-        setRating(ratingData);
+        if (parseInt(ratingData) > 0) {
+            setRating(ratingData);
+        } else {
+            setRating("-");
+        }
     }
 
     useEffect(() => {
