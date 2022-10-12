ac709c6a4bcbc08c9cba465750f5ac3bac10dc1a
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-10-12 09:58:04 +0100

    added rating to cards

--- goreview_frontend/src/components/Restaurant.js
+++ goreview_frontend/src/components/Restaurant.js
@@ -1,11 +1,23 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import RestaurantReviewPage from "./RestaurantReviewPage";
+import { useEffect, useState } from "react";
 
 
 const Restaurant = ({restaurant,reviews}) => {
 
     const route = `/${restaurant.id}`
+    const [rating, setRating] = useState(0);
 
+
+    const fetchRatingData = async () => {
+        const response = await fetch(`http://localhost:8080/restaurants/rating/${restaurant.id}`)
+        const ratingData = await response.json();
+        setRating(ratingData);
+    }
+
+    useEffect(() => {
+        fetchRatingData();
+    },[])
 
     return (
         <>
@@ -16,6 +28,7 @@ const Restaurant = ({restaurant,reviews}) => {
 
                 <p>Location: {restaurant.location}</p>
                 <p>Cuisine: {restaurant.cuisine}</p>
+                <p>Rating: {rating}</p>
             </div>
         </Link>
         </>
