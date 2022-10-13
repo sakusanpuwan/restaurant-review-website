5ca025ced0a616773f55ee160712ee88f59640b4
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-10-13 10:07:50 +0100

    added user profile

--- goreview_frontend/src/components/UserList.js
+++ goreview_frontend/src/components/UserList.js
@@ -1,6 +1,6 @@
 const UserList = ({users}) => {
     
-    const userComponents = users.map(user => <li>{user.username} Number of reviews:{user.reviews.length}</li>)
+    const userComponents = users.map(user => <li><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" height="40px"></img>{user.username} Number of reviews:{user.reviews.length}</li>)
 
     return(
         <>
